6e24bd76a6eb12e56ea1c3b8ed3f44ba34fe9ce8
Ivan Zhevniak <146636885+ivanycla@users.noreply.github.com>
2024-12-13 21:52:50 +0300

    Update 03.js

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,14 +1,12 @@
-const score =90; // тестовое значение, можно изменять
-let grade;
-
-if (score >= 90 && score <= 100) {
-    grade = 'A';
-} else if (score >= 80 && score < 90) {
-    grade = 'B';
-} else if (score >= 70 && score < 80) {
-    grade = 'C';
-} else if (score >= 50 && score < 70) {
-    grade = 'D';
-} else if (score >= 0 && score < 50) {
+if (score < 50 && score >= 0) {
     grade = 'F';
+} else if (score < 70 && score >= 50) {
+    grade = 'D';
+} else if (score < 80 && score >= 70) {
+    grade = 'C';
+} else if (score < 90 && score >= 80) {
+    grade = 'B';
+} else if (score <= 100 && score >= 90) {
+    grade = 'A';
 }
+
